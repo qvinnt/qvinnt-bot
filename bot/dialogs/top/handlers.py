9b73d20af551bd45864614d4653c3a5c2b377f77
Timofey Kochetov <herew26@gmail.com>
2025-11-14 14:44:04 +0300

    ✨ Add admin panel using top

--- bot/dialogs/top/handlers.py
+++ bot/dialogs/top/handlers.py
@@ -8,11 +8,14 @@ from bot.dialogs.top.constants import TRACKS_PER_PAGE
 from bot.services import errors
 from bot.services import track as track_service
 from bot.services import vote as vote_service
+from bot.states.admin.track import AdminTrackSG
 
 if TYPE_CHECKING:
     from aiogram_dialog import ChatEvent, DialogManager
     from aiogram_dialog.widgets.kbd import ManagedCounter, Select
     from sqlalchemy.ext.asyncio import AsyncSession
+
+    from bot.core.settings import Settings
 
 
 async def handle_start(
@@ -46,6 +49,15 @@ async def handle_track_select(
     dialog_manager: DialogManager,
     data: int,
 ) -> None:
+    settings: Settings = dialog_manager.middleware_data["settings"]
+    if event.from_user.id == settings.bot.admin_id:
+        return await dialog_manager.start(
+            AdminTrackSG.waiting_for_action,
+            data={
+                "track_id": data,
+            },
+        )
+
     session: AsyncSession = dialog_manager.middleware_data["session"]
 
     try:
@@ -56,10 +68,10 @@ async def handle_track_select(
         )
     except errors.VoteAlreadyExistsError:
         await event.answer("Вы уже проголосовали за этот трек", show_alert=True)  # pyright: ignore[reportAttributeAccessIssue]
-        return
+        return None
     except errors.ServiceError as e:
         logger.error(e)
         await event.answer("⚠️ Произошла ошибка", show_alert=True)  # pyright: ignore[reportAttributeAccessIssue]
-        return
+        return None
     else:
         await event.answer("⭐️ Вы проголосовали за трек")  # pyright: ignore[reportAttributeAccessIssue]
